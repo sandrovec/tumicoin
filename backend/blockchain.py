f37b28ef85769951635056f8dab2c217b860b011
sandrovec <sandrovec@gmail.com>
2024-12-21 11:46:41 -0500

    Create blockchain.py

---
+++ backend/blockchain.py
@@ -0,0 +1,61 @@
+import hashlib
+import json
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, transactions, timestamp=None):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.timestamp = timestamp or time.time()
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = json.dumps({
+            "index": self.index,
+            "previous_hash": self.previous_hash,
+            "transactions": self.transactions,
+            "timestamp": self.timestamp,
+            "nonce": self.nonce
+        }, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def mine_block(self, difficulty):
+        while not self.hash.startswith("0" * difficulty):
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.pending_transactions = []
+        self.difficulty = 2
+
+    def create_genesis_block(self):
+        return Block(0, "0", [], time.time())
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_transaction(self, sender, recipient, amount):
+        self.pending_transactions.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount
+        })
+
+    def mine_pending_transactions(self):
+        new_block = Block(
+            index=len(self.chain),
+            previous_hash=self.get_latest_block().hash,
+            transactions=self.pending_transactions
+        )
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+        self.pending_transactions = []
+
+# Uso de ejemplo:
+# blockchain = Blockchain()
+# blockchain.add_transaction("Alice", "Bob", 50)
+# blockchain.mine_pending_transactions()
