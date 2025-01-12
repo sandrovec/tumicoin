1115e83159caeb7dab0ff4d775feefc1663c02a6
sandrovec <sandrovec@gmail.com>
2025-01-11 23:04:46 -0500

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Wallet from "./pages/Wallet";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/wallet" element={<Wallet />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
